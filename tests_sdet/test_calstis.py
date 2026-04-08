0fa5cfdabfd911956ab1c7cbd57302e850d6175e
Spencer Ye <spencerye425@gmail.com>
2026-04-08 14:23:41 -0400

    Create new PR, test codecov comment

--- tests_sdet/test_calstis.py
+++ tests_sdet/test_calstis.py
@@ -5,9 +5,9 @@ def test_printoptions():
     calstis.prtOptions()
     assert True
 
-# def test_get_docs():
-#     calstis.getHelpAsString()
-#     assert True
+def test_get_docs():
+    calstis.getHelpAsString()
+    assert True
 
 # def test_main_no_args():
 #     with pytest.raises(SystemExit):
