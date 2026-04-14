70b24f2056237676427390802466c87e25578789
Aiden Deffner <aidenpd100@gmail.com>
2026-04-14 14:26:45 -0400

    evaldisp tests

---
+++ tests_sdet/test_evaldisp.py
@@ -0,0 +1,14 @@
+from stistools import evaldisp
+
+def test_newton():
+    coeff = [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    cenwave = 500.0
+    x = 499.0
+    wl = evaldisp.newton(x, coeff, cenwave)
+    assert wl == 500.0
+
+def test_evalDisp():
+    coeff = [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    wl = 500.0
+    pix_number = evaldisp.evalDisp(coeff, wl)
+    assert pix_number == 499.0
